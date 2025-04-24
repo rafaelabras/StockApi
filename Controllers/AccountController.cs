3dba15d3f5f92ab8667ec24de0cfa08dc92c0749
Rafael Abras <rafaelabraslessa@gmail.com>
2025-04-24 16:07:43 -0300

    feat: login

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -3,6 +3,7 @@ using aprendizahem.Interfaces;
 using aprendizahem.Models;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace aprendizahem.Controllers
 {
@@ -12,13 +13,15 @@ namespace aprendizahem.Controllers
     {
         private readonly UserManager<AppUser> _userManager;
         private readonly ITokenService _tokenService;
-        public AccountController(UserManager<AppUser> userManager, ITokenService tokenService)
+        private readonly SignInManager<AppUser> _signInManager;
+        public AccountController(UserManager<AppUser> userManager, ITokenService tokenService, SignInManager<AppUser> signing)
         {
             _userManager = userManager;
             _tokenService = tokenService;
+            _signInManager = signing;
         }
 
-        [HttpPost]
+        [HttpPost("register")]
         public async Task<IActionResult> Register([FromBody] RegisterDto registerDto)
         {
             try
@@ -62,6 +65,31 @@ namespace aprendizahem.Controllers
             {
                 return StatusCode(500, ex.Message);
             }
-        } 
+        }
+
+        [HttpPost("login")]
+        public async Task<IActionResult> Login(LoginDto login)
+        {
+            if(!ModelState.IsValid) return BadRequest(ModelState);
+
+           var user = await _userManager.Users.FirstOrDefaultAsync(x  => x.Email == login.Email);
+           if (user == null)
+                return Unauthorized("Email invalido.");
+
+            var result = await _signInManager.CheckPasswordSignInAsync(user, login.Password, false);
+
+            if (!result.Succeeded)
+                return Unauthorized("Email não encontrado ou/ Senha invalida.");
+
+            return Ok(
+                new NewUserDto
+                {
+                    UserName = user.UserName,
+                    Email = user.Email,
+                    Token= _tokenService.CreateToken(user)
+                    
+                }
+            );
+        }
     }
 }
